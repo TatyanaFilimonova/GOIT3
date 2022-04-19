dfeaafc9ba2b9442d38c645cf2849095b712d391
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2022-04-19 10:11:46 +0300

    Google Drive link

--- Project3/ImageRecognizer/model/predict_image.py
+++ Project3/ImageRecognizer/model/predict_image.py
@@ -1,7 +1,16 @@
 import numpy as np
 import tensorflow as tf
-
+from os.path import exists
 from PIL import Image
+import gdown
+
+if not exists("vgg16_cifar10_new.hdf5"):
+    url = 'https://drive.google.com/file/d/1LJnhd4X4GIjoaBv40DtgxGryT33xQykT/view?usp=sharing'
+    output = 'vgg16_cifar10_new.hdf5'
+    gdown.download(url, output, quiet=False, fuzzy=True)
+
+model_filename = "vgg16_cifar10_new.hdf5"
+model = tf.keras.models.load_model(model_filename)
 
 
 LABEL_NAMES = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
@@ -16,8 +25,6 @@ def resize_image(image_to_recognize):
 
 
 def predict_image(image):
-    model_filename = "vgg16_cifar10_new.hdf5"
-    model = tf.keras.models.load_model(model_filename)
     input_img = resize_image(image)
     predict_img = model.predict(input_img)
     pred_class = LABEL_NAMES[np.argmax(predict_img)]
